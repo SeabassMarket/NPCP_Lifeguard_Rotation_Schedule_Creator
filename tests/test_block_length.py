eed717947d87124e61095d16d6be91b35b29c740
SeabassMarket <simercado07@gmail.com>
2025-08-23 12:21:15 -0400

    feat(uses GC class): uses GS class + tested with real schedule

--- tests/test_block_length.py
+++ tests/test_block_length.py
@@ -5,6 +5,8 @@ from InfoManagers.CalculateSchedule import CalculateSchedule
 from InfoManagers.StaticAppInfo import StaticAppInfo
 from InfoManagers.Time import Time
 
+from GoogleAPICommunicators.GoogleSheetsCommunicator import GSCommunicator
+
 """THE FIRST PART OF THIS CODE IS JUST HARDCODING INFORMATION"""
 
 # Create static app info
@@ -15,17 +17,17 @@ upStands = {
     "E": [Time(hour=11, minute=0), Time(hour=20, minute=0), 1],
     "H": [Time(hour=11, minute=0), Time(hour=20, minute=0), 1],
     "K": [Time(hour=11, minute=0), Time(hour=20, minute=0), 1],
-    "A": [Time(hour=12, minute=0), Time(hour=19, minute=0), 1],
-    "B": [Time(hour=12, minute=0), Time(hour=19, minute=0), 1],
-    "I": [Time(hour=13, minute=0), Time(hour=16, minute=0), 1],
+    "A": [Time(hour=12, minute=0), Time(hour=20, minute=0), 1],
+    "B": [Time(hour=12, minute=0), Time(hour=20, minute=0), 1],
+    "I": [Time(hour=13, minute=0), Time(hour=19, minute=0), 1],
     "T": [Time(hour=13, minute=0), Time(hour=19, minute=0), 1],
     "S": [Time(hour=13, minute=0), Time(hour=19, minute=0), 1],
 }
 timelyDownStands = {
-    "SU": [Time(hour=10, minute=40), Time(hour=11, minute=0), 3],
-    "DT": [Time(hour=14, minute=0), Time(hour=14, minute=20), 2],
-    "ST": [Time(hour=15, minute=0), Time(hour=15, minute=20), 2],
-    "CU": [Time(hour=20, minute=0), Time(hour=21, minute=0), 5],
+    "SU": [Time(hour=10, minute=40), Time(hour=11, minute=0), 10],
+    "DT": [Time(hour=14, minute=0), Time(hour=14, minute=20), 3],
+    "ST": [Time(hour=15, minute=0), Time(hour=15, minute=20), 3],
+    "CU": [Time(hour=20, minute=0), Time(hour=21, minute=0), 10],
 }
 priorityDownStands = {
     "O": [Time(hour=0, minute=0), "end of day", 1],
@@ -46,19 +48,19 @@ staticAppInfo.setEventDataSpecific(standData, eventDescriptor="stand")
 
 # Create information for the stands
 lifeguards = {
-    "1": [Time(hour=10, minute=40), Time(hour=18, minute=40)],
-    "2": [Time(hour=10, minute=40), Time(hour=18, minute=40)],
-    "3": [Time(hour=10, minute=40), Time(hour=18, minute=40)],
-    "4": [Time(hour=11, minute=0), Time(hour=19, minute=0)],
-    "5": [Time(hour=11, minute=0), Time(hour=14, minute=0)],
-    "6": [Time(hour=11, minute=0), Time(hour=19, minute=0)],
-    "7": [Time(hour=12, minute=0), Time(hour=20, minute=0)],
-    "8": [Time(hour=11, minute=40), Time(hour=19, minute=40)],
-    "9": [Time(hour=13, minute=0), Time(hour=21, minute=0)],
-    "10": [Time(hour=13, minute=0), Time(hour=21, minute=0)],
-    "11": [Time(hour=13, minute=0), Time(hour=21, minute=0)],
-    "12": [Time(hour=13, minute=0), Time(hour=21, minute=0)],
-    "13": [Time(hour=13, minute=0), Time(hour=21, minute=0)],
+    "EVE": [Time(hour=10, minute=40), Time(hour=18, minute=40)],
+    "CHRISTIAN": [Time(hour=10, minute=40), Time(hour=18, minute=40)],
+    "CLARISSA": [Time(hour=11, minute=0), Time(hour=19, minute=0)],
+    "TY": [Time(hour=11, minute=0), Time(hour=19, minute=0)],
+    "LARISSA": [Time(hour=11, minute=0), Time(hour=19, minute=0)],
+    "MEGAN": [Time(hour=11, minute=0), Time(hour=19, minute=0)],
+    "JEFF": [Time(hour=12, minute=0), Time(hour=20, minute=0)],
+    "KYLE": [Time(hour=11, minute=40), Time(hour=19, minute=40)],
+    "ELLA": [Time(hour=13, minute=0), Time(hour=21, minute=0)],
+    "CARYS": [Time(hour=13, minute=0), Time(hour=21, minute=0)],
+    "ROWAN": [Time(hour=13, minute=0), Time(hour=21, minute=0)],
+    "CONNIE": [Time(hour=13, minute=0), Time(hour=21, minute=0)],
+    "GRAYSON": [Time(hour=13, minute=0), Time(hour=21, minute=0)],
 }
 
 lifeguardData = {
@@ -106,3 +108,10 @@ sortedBlockLengthKeys = sorted(list(blockLengthToCount.keys()))
 for blockLength in sortedBlockLengthKeys:
     count = blockLengthToCount[blockLength]
     print(f"There were {count} blocks with length {blockLength}")
+
+# Use GSCommunicator
+gs = GSCommunicator(staticAppInfo, calculator, 3, 1)
+
+gs.setWorksheet("Lifeguard Schedule", "NPCP_GOOGLE_SHEETS_KEY")
+gs.writeScheduleToWorksheet()
+print(f"Schedule uploaded: {gs.getItem('spreadsheet').url}")
